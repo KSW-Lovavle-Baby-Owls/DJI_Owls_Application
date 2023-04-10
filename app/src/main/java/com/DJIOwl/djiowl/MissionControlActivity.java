6a8825d410b5d48fbaf34d30ea1d588142e3d4a8
nicolehornbrook <hornbroo@purdue.edu>
2023-04-10 15:07:17 -0400

    Adding button functionality to fragment navigation arrows

--- app/src/main/java/com/DJIOwl/djiowl/MissionControlActivity.java
+++ app/src/main/java/com/DJIOwl/djiowl/MissionControlActivity.java
@@ -1,7 +1,12 @@
 package com.DJIOwl.djiowl;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 /*
 This Activity contains 4 fragments:
@@ -13,10 +18,69 @@ Each provide a new view for the user to see and control the drone's path
 There will be a dropdown to pick between manual and autonomous(mission) mode
  */
 public class MissionControlActivity extends AppCompatActivity {
+    FragmentTransaction fragmentTransaction;
+    private Fragment mFragment;
+    private ImageView leftArrowButton;
+    private ImageView rightArrowButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission_control);
+        leftArrowButton = findViewById(R.id.leftArrowButton);
+        rightArrowButton = findViewById(R.id.rightArrowButton);
+        // first fragment to display
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        mFragment = new CameraFragment(); // CreateNewNote is fragment you want to display
+        fragmentTransaction.replace(R.id.fragmentView, mFragment);
+        fragmentTransaction.commit();
+        leftArrowButton.setVisibility(View.INVISIBLE);
+
+    }
+    public void leftArrowClick(View view) {
+        if (mFragment.getClass() == CameraPlusMapFragment.class) {
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            mFragment = new MapFragment();
+            fragmentTransaction.replace(R.id.fragmentView, mFragment);
+            fragmentTransaction.commit();
+            rightArrowButton.setVisibility(View.VISIBLE);
+
+        } else if (mFragment.getClass() == MapFragment.class) {
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            mFragment = new ControlFragment();
+            fragmentTransaction.replace(R.id.fragmentView, mFragment);
+            fragmentTransaction.commit();
+
+        } else if (mFragment.getClass() == ControlFragment.class) {
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            mFragment = new CameraFragment();
+            fragmentTransaction.replace(R.id.fragmentView, mFragment);
+            fragmentTransaction.commit();
+            leftArrowButton.setVisibility(View.INVISIBLE);
+
+        }
+    }
+    public void rightArrowClick(View view) {
+        if (mFragment.getClass() == CameraFragment.class) {
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            mFragment = new ControlFragment();
+            fragmentTransaction.replace(R.id.fragmentView, mFragment);
+            fragmentTransaction.commit();
+            leftArrowButton.setVisibility(View.VISIBLE);
+
+        } else if (mFragment.getClass() == ControlFragment.class) {
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            mFragment = new MapFragment();
+            fragmentTransaction.replace(R.id.fragmentView, mFragment);
+            fragmentTransaction.commit();
+
+        } else if (mFragment.getClass() == MapFragment.class) {
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            mFragment = new CameraPlusMapFragment();
+            fragmentTransaction.replace(R.id.fragmentView, mFragment);
+            fragmentTransaction.commit();
+            rightArrowButton.setVisibility(View.INVISIBLE);
+
+        }
     }
 }
